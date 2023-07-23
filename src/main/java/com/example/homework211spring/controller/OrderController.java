4c26c1ee9dcfefee97f96141ab8ce9978a0e10e0
Mishator <voyteh89@mail.ru>
2023-07-23 12:59:14 +0300

    Initial commit

---
+++ src/main/java/com/example/homework211spring/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.example.homework211spring.controller;
+
+import com.example.homework211spring.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/add")
+    public List<Integer> addItems(@RequestParam List<Integer> items) {
+        return orderService.addAll(items);
+    }
+
+    public List<Integer> getItems() {
+        return orderService.getAll();
+    }
+}
---
+++ src/main/java/com/example/homework211spring/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.example.homework211spring.service;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Integer> addAll(List<Integer> idList);
+
+    List<Integer> getAll();
+}
